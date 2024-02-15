f2b12a13f371b7cf7ed760ce2c3c68022e0b6d99
jacopo-priori <99391573+jacopo-priori@users.noreply.github.com>
2024-02-15 13:01:17 +0100

    visualisations

--- code/file_1_a.py
+++ code/file_1_a.py
@@ -1,20 +1,54 @@
+### PACKAGES
+
 import pandas as pd
 import numpy as np 
 import statsmodels.api as sm
+import matplotlib.pyplot as plt
 
+### DATAFRAME
 
-chunks = pd.read_csv('data\Gravity_V202211.csv', chunksize=2000)
-dfs = [] # filter dataframes
-for chunk in chunks:
+chunks = pd.read_csv('C:\\Users\\HP\\Desktop\\gravity\\Gravity_V202211.csv', chunksize=2000)
+dfs = [] 
+for chunk in chunks: # filter dataframes
     df_grav = pd.DataFrame(chunk)
-    df_grav_1 = df_grav.loc[:, ['year','iso3_o', 'iso3_d', 'country_exists_o', 'country_exists_d', 'distw_arithmetic', 'gdp_ppp_pwt_o', 'gdp_ppp_pwt_d', 'tradeflow_baci', 'comlang_ethno', 'col_dep_ever', 'sibling_ever', 'contig']]  # selection of relevant variables
+    df_grav_1 = df_grav.loc[:, ['year','country_id_o', 'country_id_d', 'country_exists_o', 'country_exists_d', 'distw_harmonic', 'gdp_ppp_pwt_o', 'gdp_ppp_pwt_d', 'tradeflow_baci', 'comlang_ethno', 'heg_o', 'heg_d', 'sibling_ever', 'contig']]  # selection of relevant variables
     df_grav_2 = df_grav_1[(df_grav_1['country_exists_o'] != 0) & (df_grav_1['country_exists_d'] != 0)]  # keep only observations where both countries existed
     df_grav_3 = df_grav_2.dropna(axis=0, how='any') # drop na
-    df_grav_4 = df_grav_3[(df_grav_3['year'] > 2000)] # filter for years
+    df_grav_4 = df_grav_3[(df_grav_3['year'] >= 1997)] # filter for years
     dfs.append(df_grav_4)
 df = pd.concat(dfs)
 
-print(df.head()) 
+print(df.head(20)) 
 print(df.describe()) 
 
-df[['gdp_o', 'gdp_d', 'distw_arithmetic', 'tradeflow_baci']] = np.log(df[['gdp_ppp_pwt_o', 'gdp_ppp_pwt_d', 'distw_arithmetic', 'tradeflow_baci']]) # convert to log scale
+df[['gdp_o_log', 'gdp_d_log', 'dist_log', 'tf_log']] = np.log(df[['gdp_ppp_pwt_o', 'gdp_ppp_pwt_d', 'distw_harmonic', 'tradeflow_baci']]) # add logged values for regressions
+
+### GDP CHART
+
+df_gdp = df[['year','country_id_o','gdp_ppp_pwt_o']].drop_duplicates() # only select one observations per country per year as GDP is the same
+df_gdp = df_gdp.groupby('year')['gdp_ppp_pwt_o'].sum().reset_index() # sum GDP's per year
+print(df_gdp.head(20))
+print(df.describe()) 
+
+plt.plot(df_gdp['year'], df_gdp['gdp_ppp_pwt_o']) # plot world GDP
+plt.xlabel('Year')
+plt.ylabel('Log of sum of deflated GDP at current PPP in 2011 thousands USD')
+plt.yscale('log') # log scale
+plt.title('World GDP over time')
+plt.show()
+
+### TRADEFLOW CHART
+
+df_tf = df[['year','country_id_o', 'country_id_d', 'tradeflow_baci', 'distw_harmonic']]
+df_tf[['country_id_o', 'country_id_d']] = pd.DataFrame(np.sort(df_tf[['country_id_o', 'country_id_d']].values, axis=1), index=df.index)
+df_tf = df_tf.drop_duplicates(subset=['country_id_o', 'country_id_d'])
+df_tf = df_tf.groupby('year')[['tradeflow_baci', 'distw_harmonic']].sum().reset_index()
+print(df_tf.describe())
+print(df_tf.head(20))
+
+plt.plot(df_tf['year'], df_tf['tradeflow_baci'])
+plt.xlabel('Year')
+plt.ylabel('lorem ipsum')
+plt.yscale('log')
+plt.title('World trade flow over time')
+plt.show()
