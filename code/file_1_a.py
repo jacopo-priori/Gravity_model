376815d0362de0b0b836465c48e7f4627027b79c
jacopo-priori <99391573+jacopo-priori@users.noreply.github.com>
2024-02-15 18:09:45 +0100

    initial visualisations

--- code/file_1_a.py
+++ code/file_1_a.py
@@ -23,21 +23,7 @@ print(df.describe())
 
 df[['gdp_o_log', 'gdp_d_log', 'dist_log', 'tf_log']] = np.log(df[['gdp_ppp_pwt_o', 'gdp_ppp_pwt_d', 'distw_harmonic', 'tradeflow_baci']]) # add logged values for regressions
 
-### GDP CHART
-
-df_gdp = df[['year','country_id_o','gdp_ppp_pwt_o']].drop_duplicates() # only select one observations per country per year as GDP is the same
-df_gdp = df_gdp.groupby('year')['gdp_ppp_pwt_o'].sum().reset_index() # sum GDP's per year
-print(df_gdp.head(20))
-print(df.describe()) 
-
-plt.plot(df_gdp['year'], df_gdp['gdp_ppp_pwt_o']) # plot world GDP
-plt.xlabel('Year')
-plt.ylabel('Log of sum of deflated GDP at current PPP in 2011 thousands USD')
-plt.yscale('log') # log scale
-plt.title('World GDP over time')
-plt.show()
-
-### TRADEFLOW CHART 
+### TRADEFLOW CHART
 
 df_tf = df[['year','country_id_o', 'country_id_d', 'tradeflow_baci']]
 df_tf[['country_id_o', 'country_id_d']] = pd.DataFrame(np.sort(df_tf[['country_id_o', 'country_id_d']].values, axis=1), index=df.index) 
@@ -48,7 +34,50 @@ print(df_tf.head(20))
 
 plt.plot(df_tf['year'], df_tf['tradeflow_baci']) # plot world tradeflow
 plt.xlabel('Year')
-plt.ylabel('lorem ipsum')
+plt.ylabel('Trade flow in thousands USD')
 plt.yscale('log') # log scale
 plt.title('World trade flow over time')
 plt.show()
+
+### GDP CHART
+
+df_gdp = df[['year','country_id_o','gdp_ppp_pwt_o']].drop_duplicates() # only select one observations per country per year as GDP is the same
+df_gdp = df_gdp.groupby('year')['gdp_ppp_pwt_o'].sum().reset_index() # sum GDP's per year
+print(df_gdp.head(20))
+print(df_gdp.describe()) 
+
+plt.plot(df_gdp['year'], df_gdp['gdp_ppp_pwt_o']) # plot world GDP
+plt.xlabel('Year')
+plt.ylabel('Log of sum of deflated GDP at current PPP in 2011 thousands USD')
+plt.yscale('log') # log scale
+plt.title('World GDP over time')
+plt.show()
+
+### COUNTRIES CHART 
+
+df_ac = df[['year','country_id_o','country_exists_o']].drop_duplicates() # only select one observations per country per year as the dummy is the same
+df_ac = df_ac.groupby('year')['country_exists_o'].sum().reset_index() # amount of countries per year
+print(df_ac.head(20))
+print(df_ac.describe()) 
+
+plt.plot(df_ac['year'], df_ac['country_exists_o']) # plot world GDP
+plt.xlabel('Year')
+plt.ylabel('Amount of existing countries in df')
+plt.title('Amount of countries over time')
+plt.show()
+
+### DISTANCE CHART
+
+df_dist = df[['year','country_id_o', 'country_id_d', 'distw_harmonic']]
+df_dist[['country_id_o', 'country_id_d']] = pd.DataFrame(np.sort(df_dist[['country_id_o', 'country_id_d']].values, axis=1), index=df.index) 
+df_dist = df_dist.drop_duplicates(subset=['year','country_id_o', 'country_id_d']) # drop one observation in each pair of observations where the only difference is the direction of trade
+df_dist = df_dist.groupby('year')['distw_harmonic'].mean().reset_index() # calculate average distance per year
+print(df_dist.describe())
+print(df_dist.head(20))
+
+plt.plot(df_dist['year'], df_dist['distw_harmonic']) # plot average distance
+plt.xlabel('Year')
+plt.ylabel('Average of population-weighted harmonic mean distance in km')
+plt.title('Average distance over time')
+plt.show()
+
